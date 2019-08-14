f4f4dd67ba5d1e2e5ed03a3e2754a026b36213cc
Tom McKeesick <tmck01@gmail.com>
2019-08-14 17:38:56 +1000

    Add comments

--- generic.go
+++ generic.go
@@ -6,36 +6,32 @@ import (
 	"github.com/tmck-code/go-sortable/values/generic"
 )
 
-type CustomVal interface {
-	MyMethod() bool
-}
-
+// Let's define our own custom struct that we want to use
 type CustomMap struct {
 	mapping map[string]string
 	name string
 	hash string
 }
 
-type Custom struct {
-	mapping CustomMap
-	id int
-}
-
 func main() {
+	// Create a new "generic" container. It doesn't care what you store in it.
 	sl := new(GenericContainers.ReversibleList)
 
+	// Add some "generic" values from this lib
 	sl.Add(GenericValues.Int{Val: 1})
 	sl.Add(GenericValues.Int{Val: 100})
-	sl.Add(Custom{
-		mapping: CustomMap{
-			name: "my thing",
-			hash: "obj hash",
-			mapping: map[string]string{"key": "val"},
-		},
-		id: 123,
+
+	// Add our own custom struct
+	sl.Add(CustomMap{
+		name: "my thing",
+		hash: "obj hash",
+		mapping: map[string]string{"key": "val"},
 	})
+	// Check that everything was added to the container
 	fmt.Printf("original: %+v\n", sl)
 
+	// Now, reverse the contents of the list!
+	// We don't need to implement anything for this to happen.
 	sl.Reverse()
 	fmt.Printf("reversed: %+v\n", sl)
 }
--- sortable.go
+++ sortable.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
+	// Create a new "sortable" container
+	// Objects only need to implement the Value() and Compare() methods,
+	// in order to provide a sortable value to the container
+	// This method works with things like sets - could define Hash()
 	s := new(SortableContainers.SortableList)
 
+	// Add our "sortable" values to the container
 	s.Add(SortableValues.Float{Fval: 2})
 	s.Add(SortableValues.Int{Val: 1})
+	// Check that everything was added to the container
 	fmt.Printf("original: %+v\n", s)
 
+	// Sort the contents!
 	s.Sort()
 	fmt.Printf("sorted:   %+v\n", s)
 }
